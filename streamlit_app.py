ecd65823c837189c592918d6878eea451dde32f6
emilyhey <78817893+emilyhey@users.noreply.github.com>
2023-06-02 21:44:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+from tkinter import *
 
-st.write("Hello World!")
+window = Tk()
+window.geometry("500x150")
+frame = frame(window, anchor = S)
+frame.pack()
+
+leftframe = Frame(window, bg = "black", bd = "5")
+leftframe.pack(side=LEFT)
+ 
+rightframe = Frame(window)
+rightframe.pack(side=RIGHT)
+ 
+label = Label(frame, text = "fun fact", wraplength = 1)
+label.pack()
+
+button = Button(rightframe, text="click for another", command=button_click, font="white")
+button.pack()
+
+window.mainloop()
